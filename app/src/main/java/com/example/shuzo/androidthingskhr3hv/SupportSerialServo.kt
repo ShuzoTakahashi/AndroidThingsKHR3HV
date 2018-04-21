9e1167e725f807b302d22d3523f21c100f1f33dc
ShuzoTakahashi <shuzo.create@gmail.com>
2018-04-21 13:38:31 +0900

    サーボ動作部をクラスにまとめた

--- app/src/main/java/com/example/shuzo/androidthingskhr3hv/SupportSerialServo.kt
+++ app/src/main/java/com/example/shuzo/androidthingskhr3hv/SupportSerialServo.kt
@@ -4,7 +4,7 @@ import android.os.Handler
 import android.os.HandlerThread
 import android.util.Log
 import com.google.android.things.pio.Gpio
-import com.google.android.things.pio.PeripheralManagerService
+import com.google.android.things.pio.PeripheralManager
 import com.google.android.things.pio.UartDevice
 import java.io.IOException
 import kotlin.experimental.or
@@ -13,31 +13,26 @@ import kotlin.experimental.or
  * Created by shuzo on 2018/03/07.
  */
 
-class SupportSerialServo {
+class SupportSerialServo(private val service:PeripheralManager,val handler: Handler) {
     private var serialServo: UartDevice? = null
-    private val service = PeripheralManagerService()
 
-    private var enPin: Gpio = service.openGpio("BCM4") //送受信切り替えピン
+    private var enPin: Gpio = service.openGpio("BCM4") //送受信切り替えピン HIGHで送信,LOWで受信
     private var uartComThread: HandlerThread = HandlerThread("UartComThread")
     private var uartComHandler: Handler
     private val TAG = "UartThread"
 
     init {
-        // TODO: Uartを別スレッドで扱う必要はある？ メインはUIを持たないので別に不要では？
         uartComThread.start()
         uartComHandler = Handler(uartComThread.looper)
 
-        // TODO : 送信するだけならHIGHのままでよい？
-        enPin.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH) // HIGHの時送信
-
         serialServo = try {
-            service.openUartDevice("UART0").also {
+            service.openUartDevice("UART0").also { uartDevice ->
                 // TODO: プロパティ形式で代入できないのはなぜ？
-                it.setBaudrate(BAUD_RATE) //通信速度
-                it.setDataSize(DATA_BITS) //ビット長
-                it.setParity(UartDevice.PARITY_EVEN) //偶数パリティ
-                it.setStopBits(STOP_BITS) //ストップビット
-                it.setHardwareFlowControl(UartDevice.HW_FLOW_CONTROL_NONE) //フロー制御なし
+                uartDevice.setBaudrate(BAUD_RATE) //通信速度
+                uartDevice.setDataSize(DATA_BITS) //ビット長
+                uartDevice.setParity(UartDevice.PARITY_EVEN) //偶数パリティ
+                uartDevice.setStopBits(STOP_BITS) //ストップビット
+                uartDevice.setHardwareFlowControl(UartDevice.HW_FLOW_CONTROL_NONE) //フロー制御なし
             }
         } catch (e: IOException) {
             Log.e(TAG, "Unable to open UART device", e)
@@ -45,34 +40,39 @@ class SupportSerialServo {
         }
     }
 
-    fun servoToRotate(id: Byte, rotate: Int) {
+    fun toPosData(id: Int, PosData: Int) {
+
         if (serialServo != null) {
+            try {
+                enPin.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH) // HIGHにセット(送信)
+                uartComHandler.post {
 
-            uartComHandler.post {
+                    val cmd = ByteArray(3)
+                    cmd[0] = 0b10000000.toByte() or id.toByte() // 0x80でポジション
+                    cmd[1] = ((PosData shr 7) and 0x007f).toByte() //POS_H
+                    cmd[2] = (PosData and 0x007F).toByte() // POS_L
 
-                // TODO : ↑グローバルな値に保存するのは正しくない？
+                    // Log 出力
+                    val afPos = (cmd[1].toInt() shl 7) or cmd[2].toInt()
+                    Log.d("id", id.toString())
+                    Log.d("pos", afPos.toString())
 
-                val doublPos: Double = (rotate.toDouble() / 270.0) + 0.5
-                val pos = ((doublPos * 4000) + 5500).toInt()
-
-                //サーボ０に0°を出す
-                var cmd = ByteArray(3)
-                cmd[0] = 0b10000000.toByte() or id // 0x80でポジション
-                cmd[1] = ((pos shr 7) and 0x007f).toByte() //POS_H
-                cmd[2] = (pos and 0x007F).toByte() // POS_L
-
-                // Log 出力
-                val afPos = (cmd[1].toInt() shl 7) or cmd[2].toInt()
-                Log.d("id", id.toString())
-                Log.d("rotate", rotate.toString())
-                Log.d("pos", afPos.toString())
-
-                serialServo!!.write(cmd, cmd.size)
-                serialServo!!.flush(UartDevice.FLUSH_OUT)
+                    serialServo?.write(cmd, cmd.size)
+                    serialServo?.flush(UartDevice.FLUSH_OUT)
+                }
+            } catch (e: IOException) {
+                //TODO
             }
+
         } else {
             Log.e(TAG, "Unable to open UART device")
         }
+    }
+
+    fun toRotate(id: Int, rotate: Int) {
+        val parRotate: Double = (rotate.toDouble() / 270.0) + 0.5
+        val pos = ((parRotate * 4000) + 5500).toInt()
+        toPosData(id,pos)
     }
 
     fun close() {
