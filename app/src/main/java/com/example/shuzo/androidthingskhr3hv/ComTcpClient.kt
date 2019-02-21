3a62391a170ed598bd84eeda89497c9842321f98
ShuzoTakahashi <shuzo.create@gmail.com>
2019-02-21 16:34:37 +0900

    update: coroutinesの使用

--- app/src/main/java/com/example/shuzo/androidthingskhr3hv/ComTcpClient.kt
+++ app/src/main/java/com/example/shuzo/androidthingskhr3hv/ComTcpClient.kt
@@ -1,69 +1,83 @@
 package com.example.shuzo.androidthingskhr3hv
 
-import android.os.Handler
-import android.os.HandlerThread
-import android.util.Log
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 import java.io.*
 import java.net.Socket
 import java.net.UnknownHostException
+import kotlin.coroutines.CoroutineContext
 
 /**
- * Created by shuzotakahashi on 2018/04/21.
+ * Created by shuzotakahashi on 2018/01/13.
  */
 
-class ComTcpClient(ip: String, port: Int, val uiHandler: Handler) {
+class ComTcpClient(private val ip: String, private val port: Int, private val channel: Channel<Enum<ComState>>) :
+        CoroutineScope {
 
-    private var tcpHandler: Handler
-    private lateinit var socket: Socket
-    private val TAG = ComTcpClient::class.java.simpleName
+    private val job = Job()
 
-    private val createSocket = Runnable {
-        try {
-            socket = Socket(ip, port)
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Default
 
-            val message = uiHandler.obtainMessage(MSG_CONNECTION_SUCCESS)
-            uiHandler.sendMessage(message)
+    enum class ComState {
+        MSG_CONNECTION_SUCCESS, MSG_CONNECTION_FAILED, MSG_IOEXCEPTION
+    }
 
-        } catch (e: IOException) {
-            Log.e(TAG, "IOException", e)
-            uiHandler.sendMessage(uiHandler.obtainMessage(MSG_CONNECTION_FAILED))
+    private var socket: Socket? = null
 
-        } catch (e: UnknownHostException) {
-            Log.e(TAG, "UnknownHostException", e)
-            uiHandler.sendMessage(uiHandler.obtainMessage(MSG_CONNECTION_FAILED))
+    var isConnected: Boolean = false
+        get() = socket?.isConnected ?: false
+
+    fun connect() {
+        runBlocking {
+            println("接続開始...")
+            try {
+                socket = Socket(ip, port)
+                channel.send(ComState.MSG_CONNECTION_SUCCESS)
+
+            } catch (e: IOException) {
+                println("IOException")
+                channel.send(ComState.MSG_IOEXCEPTION)
+
+            } catch (e: UnknownHostException) {
+                println("UnknownHostException")
+                channel.send(ComState.MSG_CONNECTION_FAILED)
+            }
         }
     }
 
-    init {
-        val thread = HandlerThread("TcpComThread")
-        thread.start()
-        tcpHandler = Handler(thread.looper)
-        tcpHandler.post(createSocket)
-    }
-
-    // TODO : リネーム
-    fun actionInOut(func: (OutputStream, InputStream) -> Unit) {
-        try {
-            if (socket.isConnected) {
-                tcpHandler.post { func(socket.outputStream, socket.inputStream) }
-            } else {
-                Log.e(TAG, "接続されていない。")
-                uiHandler.sendMessage(uiHandler.obtainMessage(MSG_CONNECTION_FAILED))
-                throw IllegalStateException()
+    // TODO: リネーム
+    fun getIO(func: (OutputStream, InputStream) -> Unit) {
+        if (socket == null) throw IllegalStateException()
+        socket?.also { socket ->
+            launch {
+                try {
+                    if (socket.isConnected) {
+                        func(socket.outputStream, socket.inputStream)
+                    } else {
+                        println("接続されていない")
+                        channel.send(ComState.MSG_CONNECTION_FAILED)
+                        throw IllegalStateException()
+                    }
+                } catch (e: IOException) {
+                    channel.send(ComState.MSG_IOEXCEPTION)
+                }
             }
-        } catch (e: IOException) {
-            uiHandler.sendMessage(uiHandler.obtainMessage(MSG_BT_IOEXCEPTION))
         }
     }
 
     fun close() {
-        try {
-            if (socket.isConnected) {
-                socket.close()
+        if (socket == null) throw IllegalStateException()
+        socket?.also { socket ->
+            runBlocking {
+                try {
+                    if (socket.isConnected) socket.close()
+
+                } catch (e: IOException) {
+                    println("close()にてエラー発生")
+                    channel.send(ComState.MSG_IOEXCEPTION)
+                }
             }
-        } catch (e: IOException) {
-            Log.e(TAG, "close()メソッドにてエラー発生")
-            uiHandler.sendMessage(uiHandler.obtainMessage(MSG_BT_IOEXCEPTION))
         }
     }
 
